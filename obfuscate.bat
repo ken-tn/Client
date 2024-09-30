463b724835df67769595108bcc130461a7d90310
kent <kent911t@gmail.com>
2024-09-30 05:50:59 +0100

    working parameters

--- obfuscate.bat
+++ obfuscate.bat
@@ -1,7 +1,7 @@
 :: npm exec to enter environment
 @echo off
-SET /A seed=3022
+SET /A seed=30223
 
 for /f "tokens=*" %%f in (obfuscatelist.txt) do (
-    javascript-obfuscator %%f -o %%f --seed=%seed% --compact true --rename-properties true --self-defending false --split-strings true
+    javascript-obfuscator %%f -o %%f --seed=%seed% --string-array-threshold 0.46 --string-array-wrappers-count 3
 )
