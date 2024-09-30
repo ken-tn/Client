b2b984c7ac0cfd78a940224f455ba0355757d0d5
kent <kent911t@gmail.com>
2024-09-30 05:28:11 +0100

    good options

--- obfuscate.bat
+++ obfuscate.bat
@@ -3,5 +3,5 @@
 SET /A seed=3022
 
 for /f "tokens=*" %%f in (obfuscatelist.txt) do (
-    javascript-obfuscator %%f -o %%f --seed=%seed% --compact true
+    javascript-obfuscator %%f -o %%f --seed=%seed% --compact true --rename-properties true --self-defending false --split-strings true
 )
